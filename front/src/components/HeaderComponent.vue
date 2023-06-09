f397416f2e60ae2a5aacf1b6fecd9128288891cb
Leowenex <antoine.craipeau@efrei.net>
2023-06-09 15:02:34 +0200

    HeaderComponent

---
+++ front/src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+        <h1>E-COURTAGE</h1>
+        <button>Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    components: {
+    }
+}
+</script>
+
+<style>
+#header {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
--- front/src/views/ClientHomeView.vue
+++ front/src/views/ClientHomeView.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
+        <HeaderComponent/>
         <h1>This is a client home page</h1>
     </div>
 </template>
 
 <script>
-export default {
+import HeaderComponent from '@/components/HeaderComponent.vue'
 
+export default {
+    name: 'ClientHomeView',
+    components: {
+        HeaderComponent
+    },
 }
 </script>
 
