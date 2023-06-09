c806fa7f3506da4526ccebd1ec37dc9d18c4f391
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-06-09 21:25:59 +0200

    update

--- back/controllers/demande.js
+++ back/controllers/demande.js
@@ -1,5 +1,5 @@
 const Sequelize = require("../db.connection");
-const Demande = require("../models/demande.model.js")(Sequelize.connection, Sequelize.library);
+const Demande = require("../models/Demande.model.js")(Sequelize.connection, Sequelize.library);
 
 exports.createDemande = async (req, res) => {
     // Create new Demande
