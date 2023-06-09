79373893a02f3bd18da8ae5cdcf1cfaa65ccf5f1
Leowenex <antoine.craipeau@efrei.net>
2023-06-09 18:38:05 +0200

    Prototype client home page

--- front/src/components/ClientDemandForm.vue
+++ front/src/components/ClientDemandForm.vue
@@ -1,15 +1,100 @@
 <template>
     <div>
-        <h1>This is a demand form filling pop up</h1>
+        <!--<h1>This is a demand form filling pop up</h1>-->
+        <button @click="()=>handleDisplay()">NEW DEMAND</button>
+        <div id="modal-form" ref="modalForm" v-if="displayForm">
+            <h2>New Demand</h2>
+            <form id="form">
+                <div class="mb-3">
+                    <label for="demandSubject" class="form-label">Sujet</label>
+                    <input type="text" class="form-control" id="demandSubject" v-model="subject">
+                </div>
+                <div class="mb-3">
+                    <label for="demandAmount" class="form-label">Amount</label>
+                    <input type="text" class="form-control" id="demandAmount" v-model="amount">
+                </div>
+                <div class="mb-3">
+                    <label for="demandDuration" class="form-label">Durée</label>
+                    <input type="text" class="form-control" id="demandDuration" v-model="duration">
+                </div>
+                <div class="mb-3">
+                    <label for="demandPurpose" class="form-label">Raison</label>
+                    <input type="text" class="form-control" id="demandPurpose" v-model="purpose">
+                </div>
+                <div class="mb-3">
+                    <label for="formFileMultiple" class="form-label">Vos fichiers</label>
+                    <input class="form-control" type="file" id="formFileMultiple" multiple v-on:change="(ev)=>onFilesChange(ev)">
+                </div>
+                <button type="submit" class="btn btn-primary" @click="(ev)=>handleSubmit(ev)">Envoyer</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
 
+export default {
+    name: 'ClientDemandForm',
+    data() {
+        return {
+            subject: '',
+            amount: '',
+            duration: '',
+            purpose: '',
+            displayForm: false,
+            files: []
+        }
+    },
+    methods: {
+        handleSubmit(ev) {
+            ev.preventDefault();
+            console.log(this.files);
+            //Send data to the server with XMLHttpRequest
+            let formData = new FormData();
+            formData.append('subject', this.subject);
+            formData.append('amount', this.amount);
+            formData.append('duration', this.duration);
+            formData.append('purpose', this.purpose);
+            formData.append('files', this.files);
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://localhost:3000/api/demand');
+            xhr.send(formData);
+            xhr.onload = () => {
+                console.log(xhr.response);
+            }
+        },
+        handleDisplay() {
+            this.displayForm = !this.displayForm;
+        },
+        onFilesChange(ev) {
+            this.files = ev.target.files;
+        }
+    }
 }
 </script>
 
 <style>
+
+    #modal-form{
+        color: black;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #D9D9D9;
+        padding: 50px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.5);
+        width: 50vw;
+        height: 70vh;
+        border-radius: 10px;
+    }
+
+    #form{
+        margin-top: 50px;
+        text-align: left;
+    }
+
+
 
 </style>
--- front/src/views/ClientHomeView.vue
+++ front/src/views/ClientHomeView.vue
@@ -1,21 +1,145 @@
 <template>
     <div>
         <HeaderComponent/>
-        <h1>This is a client home page</h1>
+        <!--<h1>This is a client home page</h1>-->
+        <div id="main-body">
+            <div id="body-left">
+                <div id="profile-overview-area">
+                    <h2>Votre profil</h2>
+
+                </div>
+                <div id="notifications-view-area">
+                    <h2>Vos notifications</h2>
+                    <ul id="notifications-list">
+                        <li>Notification 1</li>
+                        <li>Notification 2</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="body-right">
+                <div id="demands-overview-area">
+                    <h2>Liste de vos demandes en cours</h2>
+                    <ul id="demands-list">
+
+                    </ul>
+                </div>
+                <div id="add-demand-area">
+                    <ClientDemandForm/>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import ClientDemandForm from '@/components/ClientDemandForm.vue';
 import HeaderComponent from '@/components/HeaderComponent.vue'
 
 export default {
     name: 'ClientHomeView',
     components: {
-        HeaderComponent
-    },
+    HeaderComponent,
+    ClientDemandForm
+},
 }
 </script>
 
 <style>
+    #main-body {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        width: 95%;
+        height: 90%;
+        height: 80vh;
+        margin: auto;
+    }
+
+    /* Left Part Stuff */
+    
+    #body-left {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 40%;
+        height: 100%;
+    }
+
+    #profile-overview-area {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 100%;
+        height: 50%;
+        border: 1px solid black;
+    }
+
+    #notifications-view-area {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 100%;
+        height: 50%;
+        border: 1px solid black;
+    }
+
+    #notifications-list {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 100%;
+        height: 100%;
+        border: 1px solid black;
+        background-color: #D9D9D9;
+    }
+
+    /* Right Part Stuff */
+
+    #body-right {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 55%;
+        height: 100%;
+    }
+
+    #demands-overview-area {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 100%;
+        height: 85%;
+        border: 1px solid black;
+    }
+
+    #demands-list {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: flex-start;
+        width: 100%;
+        height: 100%;
+        border: 1px solid black;
+        background-color: #D9D9D9;
+    }
+
+    #add-demand-area {
+        display: flex;
+        flex-direction: column;
+        justify-content: right;
+        align-items: flex-end;
+        width: 100%;
+        height: 15%;
+        border: 1px solid black;
+    }
+
 
 </style>
