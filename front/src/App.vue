2eb122b8539ec895463612d0d5870ab826e73a19
Leowenex <antoine.craipeau@efrei.net>
2023-06-10 13:58:41 +0200

    Using Roboto font on every page ?

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,8 +3,11 @@
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
