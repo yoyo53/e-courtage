24f25e96c621c9e8ff3ec5118e04f43bb53385e3
yoyo53 <yoyo53@users.noreply.github.com>
2023-06-13 15:42:26 +0000

    deploy: 7a48ad65d76c786aa14c5ed20d77c38b620b8a94

--- back/controllers/demande_banque.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require("../db.connection");
-const Demande = require("../models/Demande.model.js")(Sequelize.connection, Sequelize.library);
-const sessions = require("./session.js");
-
-//TODO
--- back/models/contient.model.js
+++
@@ -1,6 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Contient = sequelize.define("contient", {
-        
-    }, {timestamps: false});
-    return Contient;
-};
