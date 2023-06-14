6152df700120c09c3627e51baf1730b27f58d84d
Thomas Song <thomas.song@efrei.net>
2023-06-14 14:05:29 +0200

    banque demande handling controller

--- back/controllers/demande_banque.js
+++ back/controllers/demande_banque.js
@@ -1,5 +1,80 @@
 const Sequelize = require("../db.connection");
 const Demande = require("../models/Demande.model.js")(Sequelize.connection, Sequelize.library);
+const Banque = require("../models/banque.model.js")(Sequelize.connection, Sequelize.library);
+const Accepter = require("../models/accepter.model.js")(Sequelize.connection, Sequelize.library);
 const sessions = require("./session.js");
+const file = require("./file.js");
 
-//TODO
+exports.getAllDemandes = async(req, res) => {
+    // Check if token is valid
+    var token = req.get("Authorization");
+
+    let session = await sessions.verifyToken(token, "banque");
+
+    if(!session){
+        res.status(401).send({ message: "Unauthorized" });
+        return;
+    }else{
+        var banque = await sessions.findByToken(token, "banque");
+        // Get all demandes
+        let accepters = await Accepter.findAll({ where: { id_banque: banque.id_banque, status : {[Op.not] : -1} }});
+        let demandes = await Demande.findAll({ where: { id_demande: accepters.id_demande } });
+        res.status(200).send(demandes);
+    }
+}
+
+exports.getSingleDemande = async(req, res) => {
+    // Check if token is valid
+    var token = req.get("Authorization");
+
+    let session = await sessions.verifyToken(token, "banque");
+
+    if(!session){
+        res.status(401).send({ message: "Unauthorized" });
+        return;
+    }else{
+        var banque = await sessions.findByToken(token, "banque");
+        // Get single demande
+        let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
+        let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
+        res.status(200).send(demande);
+    }
+}
+
+exports.downloadDemande = async(req, res) => {
+    // Check if token is valid
+    var token = req.get("Authorization");
+
+    let session = await sessions.verifyToken(token, "banque");
+
+    if(!session){
+        res.status(401).send({ message: "Unauthorized" });
+        return;
+    }else{
+        var banque = await sessions.findByToken(token, "banque");
+        // Get single demande
+        let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
+        let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
+        //file.downloadFile(res, demande.path);
+    }
+}
+
+exports.updateDemande = async(req, res) => {
+    // Check if token is valid
+    var token = req.get("Authorization");
+
+    let session = await sessions.verifyToken(token, "banque");
+
+    if(!session){
+        res.status(401).send({ message: "Unauthorized" });
+        return;
+    }else{
+        var banque = await sessions.findByToken(token, "banque");
+        // Get single demande
+        let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
+        for(var key in req.body){
+            accepter[key] = req.body[key];
+        }
+        await accepter.save();
+    }
+}
--- back/routes/demande_banque.js
+++ back/routes/demande_banque.js
@@ -3,16 +3,16 @@ var demande_banque = require('../controllers/demande_banque');
 var router = express.Router();
 
 /*  Récupération des demandes d'une banque */
-//router.get('/getAllDemandes', demande_banque.getAllDemandes);
+router.get('/getAllDemandes', demande_banque.getAllDemandes);
 
 /*  Récupération d'une demande d'une banque */
-//router.get('/getSingleDemande/:id_demande', demande_banque.getSingleDemande);
+router.get('/getSingleDemande/:id_demande', demande_banque.getSingleDemande);
 
 /* Telechargement d'une demande */
-//router.get('/downloadDemande/:id_demande', demande_banque.downloadDemande);
+router.get('/downloadDemande/:id_demande', demande_banque.downloadDemande);
 
 /* Modification d'une demande */
-//router.patch('/updateDemande/:id_demande', demande_banque.updateDemande);
+router.patch('/updateDemande/:id_demande', demande_banque.updateDemande);
 
 
 
