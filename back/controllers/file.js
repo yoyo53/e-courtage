7d3c809728bc68a59cf716dd980fe87f1e339ac0
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-06-15 11:45:08 +0200

    update upload et download firebase

--- back/controllers/file.js
+++ back/controllers/file.js
@@ -2,7 +2,7 @@ const bucket = require('./firebase.connection');
 const JSZip = require('jszip');
 
 
-async function generateSignedUrl(request, response) {
+async function generateSignedUrl(filepath, response) {
 	const options = {
 		version: 'v2',
 		action: 'read',
@@ -12,20 +12,20 @@ async function generateSignedUrl(request, response) {
 	response.status(200).send(`URL du fichier: ${url}`);
 };
 
-async function uploadFile(request, response) {
-    await bucket.file(request.file.originalname).save(request.file.buffer, { contentType: request.file.mimetype }, (error) => {
+async function uploadFile(request, filepath, response) {
+    await bucket.file(filepath).save(request.file.buffer, { contentType: request.file.mimetype }, (error) => {
         if (error) { throw error }
         response.status(200).send(`${request.file.originalname} uploaded to bucket.`);
     })
 }
 
-async function getFile(request, response) {
-    bucket.file(request.params.filepath).createReadStream().pipe(response);
+async function getFile(filepath, response) {
+    bucket.file(filepath).createReadStream().pipe(response);
 }
 
-async function getFiles(request, response) {
+async function getFiles(filepaths, response) {
 	const zip = new JSZip();
-	for (filepath of JSON.parse(request.params.filepaths)) {
+	for (let filepath of JSON.parse(filepaths)) {
 		zip.file(filepath, bucket.file(filepath).createReadStream(), { binary : true });
 	}
 	zip.generateNodeStream().pipe(response);
