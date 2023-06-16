35b2a985306e7cef75dd114c946601c00fbca0ee
Thomas Song <thomas.song@efrei.net>
2023-06-16 12:00:02 +0200

    try catch for demande and document

--- back/controllers/demande_banque.js
+++ back/controllers/demande_banque.js
@@ -1,73 +1,77 @@
 const Sequelize = require("../db.connection");
 const Demande = require("../models/demande.model.js")(Sequelize.connection, Sequelize.library);
 const Banque = require("../models/banque.model.js")(Sequelize.connection, Sequelize.library);
+const Document = require("../models/document.model.js")(Sequelize.connection, Sequelize.library);
 const Accepter = require("../models/accepter.model.js")(Sequelize.connection, Sequelize.library);
 const sessions = require("./session.js");
-const file = require("./file.js");
 const { Op } = require("sequelize");
 
 exports.getAllDemandes = async(req, res) => {
-    // Check if token is valid
-    var token = req.get("Authorization");
+    try{
+        // Check if token is valid
+        var token = req.get("Authorization");
 
-    let session = await sessions.verifyToken(token, "banque");
+        let session = await sessions.verifyToken(token, "banque");
 
-    if(!session){
-        res.status(401).send({ message: "Unauthorized" });
-        return;
-    }else{
-        try{
-            var banque = await sessions.findByToken(token, "banque");
-            // Get all demandes
-            let accepters = await Accepter.findAll({ where: { id_banque: banque.id_banque, statut : {[Op.not] : -1} }});
-            let demandes = await Demande.findAll({ where: { id_demande: accepters.id_demande } });
-            res.status(200).send(demandes);
+        if(!session){
+            res.status(401).send({ message: "Unauthorized" });
+            return;
+        }else{
+            try{
+                var banque = await sessions.findByToken(token, "banque");
+                // Get all demandes
+                let accepters = await Accepter.findAll({ where: { id_banque: banque.id_banque, statut : {[Op.not] : -1} }});
+                let demandes = [];
+                for(let accepter of accepters){
+                    let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
+                    demandes.push(demande);
+                }
+                
+                res.status(200).send(demandes);
+            }
+            catch(err){
+                res.status(500).send({ message: "Error has occured" });
+            }
+            
         }
-        catch(err){
-            res.status(500).send({ message: err.message });
-        }
-        
+    } catch(err){
+        res.status(500).send({ message: "Error has occured" });
     }
+    
 }
 
 exports.getSingleDemande = async(req, res) => {
-    // Check if token is valid
-    var token = req.get("Authorization");
+    try{
+        // Check if token is valid
+        var token = req.get("Authorization");
 
-    let session = await sessions.verifyToken(token, "banque");
+        let session = await sessions.verifyToken(token, "banque");
 
-    if(!session){
-        res.status(401).send({ message: "Unauthorized" });
-        return;
-    }else{
-        var banque = await sessions.findByToken(token, "banque");
-        // Get single demande
-        let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
-        let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
-        res.status(200).send(demande);
-        
+        if(!session){
+            res.status(401).send({ message: "Unauthorized" });
+            return;
+        }else{
+            var banque = await sessions.findByToken(token, "banque");
+            // Get single demande
+            let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
+            let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
+            res.status(200).send(demande);
+            
+        }
     }
+    catch(err){
+        res.send(500).send({ message: "Error has occured" });
+    }
+
 }
 
-exports.downloadDemande = async(req, res) => {
-    // Check if token is valid
-    var token = req.get("Authorization");
-
-    let session = await sessions.verifyToken(token, "banque");
-
-    if(!session){
-        res.status(401).send({ message: "Unauthorized" });
-        return;
-    }else{
-        var banque = await sessions.findByToken(token, "banque");
-        // Get single demande
-        let accepter = await Accepter.findOne({ where: { id_banque: banque.id_banque, id_demande: req.params.id_demande } });
-        let demande = await Demande.findOne({ where: { id_demande: accepter.id_demande } });
-        //file.downloadFile(res, demande.path);
-    }
-}
 
 exports.updateDemande = async(req, res) => {
+    try{
+
+    } catch(err){
+        res.status(500).send({ message: "Error has occured" });
+    }
     // Check if token is valid
     var token = req.get("Authorization");
 
@@ -84,5 +88,6 @@ exports.updateDemande = async(req, res) => {
             accepter[key] = req.body[key];
         }
         await accepter.save();
+        res.status(200).send({ message: "Demande updated successfully" });
     }
 }
