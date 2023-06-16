a24feebf44073e35039cdc28d3701b3a4c410139
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2023-06-16 09:47:09 +0200

    Update client.js

--- back/controllers/client.js
+++ back/controllers/client.js
@@ -4,4 +4,64 @@ const Sequelize = require("../db.connection");
 const Client = require("../models/client.model.js")(Sequelize.connection, Sequelize.library);
 
 /* END db initialization */
+import { getUsers, getUserById, insertUser, updateUserById, deleteUserById } from "../models/userModel.js";
 
+// Get All users
+export const showUsers = (req, res) => {
+    getUsers((err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// Get Single user 
+export const showUserById = (req, res) => {
+    const id = req.params.id;
+    getUserById(id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// Create New user
+export const createUser = (req, res) => {
+    const data = req.body;
+    insertUser(data, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// Update User
+export const updateUser = (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    updateUserById(data, id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// Delete User
+export const deleteUser = (req, res) => {
+    const id = req.params.id;
+    deleteUserById(id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
