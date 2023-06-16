725ed04812f6f6621700ce1d63e3888e4dcb72ec
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-06-16 10:46:06 +0200

    update cors policy

--- back/app.js
+++ back/app.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 
 
 app.listen(PORT, (error) =>{
