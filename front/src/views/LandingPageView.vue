11989d8ccdc4a274ca0c310735a5f761ff0e47d1
Romain Foucher <58913880+ReynauldTheHopeless@users.noreply.github.com>
2023-06-20 00:14:16 +0200

    Landing Page V2

--- front/src/views/LandingPageView.vue
+++ front/src/views/LandingPageView.vue
@@ -1,114 +1,134 @@
 <template>
-  <div>
-    <header-component/>
-    <h1>Trouvez votre prêt immobilier rapidement et facilement !</h1>
+  <div class="landing-page-container">
+    <header-component />
+    <div class="landing-page">
+      <header>
+        <h1>Trouvez votre prêt immobilier idéal avec notre plateforme de confiance !</h1>
+        <h2>Connectez-vous avec des banques compétitives et trouvez les meilleures offres de prêt en quelques clics.</h2>
+      </header>
 
-    <img src="logo.jpg" alt="Logo">
+      <section class="illustration">
+        <img src="XXXXXX" alt="Photo d'une famille" />
+      </section>
 
-    <div class="info-block">
-      <p>Déposez votre demande de prêt en ligne et obtenez des offres de prêt compétitives en quelques clics seulement.</p>
-      <p>Connectez-vous directement avec les institutions financières partenaires pour gagner du temps et de l'argent dans votre recherche de prêt immobilier.</p>
+      <section class="benefits">
+        <h3>Avantages clés</h3>
+        <ul>
+          <li>Des prêts immobiliers sur mesure pour répondre à vos besoins</li>
+          <li>Accès direct aux offres de plusieurs institutions financières</li>
+          <li>Processus simple et rapide de dépôt des pièces et d'évaluation des dossiers</li>
+          <li>Gain de temps et d'efforts grâce à une recherche centralisée</li>
+        </ul>
+      </section>
+
+      <section class="testimonials">
+        <h3>Témoignages</h3>
+        <div class="testimonial">
+          <img src="client1.jpg" alt="Client 1" />
+          <p>"Grâce à cette plateforme, j'ai pu obtenir un prêt immobilier avec un taux avantageux et sans tracas. Je la recommande vivement !"</p>
+          <span>- Jean-Michel</span>
+        </div>
+        <div class="testimonial">
+          <img src="client2.jpg" alt="Client 2" />
+          <p>"Je suis impressionné par la facilité d'utilisation de cette plateforme. J'ai rapidement trouvé l'offre de prêt qui me convenait."</p>
+          <span>- Matthieu Evuort</span>
+        </div>
+      </section>
+
+      <section class="call-to-action">
+        <h3>Rejoignez-nous !</h3>
+        <div class="cta-buttons">
+          <button class="cta-button">Professionnels</button>
+          <button class="cta-button">Particuliers</button>
+        </div>
+      </section>
+
+      <section class="reassurance">
+        <h3>Nos partenaires</h3>
+        <div class="partner-logos">
+          <img src="bank1.png" alt="Logo SG" />
+          <img src="bank2.png" alt="Logo BNP Paribas" />
+          <img src="bank3.png" alt="Logo BRED" />
+        </div>
+        <p>Plus de XXXX prêts accordés avec succès</p>
+        <p>Taux de satisfaction client de XX%</p>
+      </section>
     </div>
-
-    <form class="loan-form">
-      <label for="loan-amount">Montant du prêt :</label>
-      <input type="text" id="loan-amount">
-
-      <label for="loan-duration">Durée du prêt :</label>
-      <input type="text" id="loan-duration">
-
-      <label for="property-type">Type de bien immobilier :</label>
-      <select id="property-type">
-        <option value="apartment">Appartement</option>
-        <option value="house">Maison</option>
-        <option value="land">Terrain</option>
-      </select>
-
-      <label for="contact">Vos coordonnées :</label>
-      <input type="text" id="contact">
-
-      <button class="cta-button">Commencer maintenant</button>
-    </form>
-
-    <div class="testimonials">
-      <div class="testimonial">
-        <img src="customer1.jpg" alt="Photo du client">
-        <p>"Grâce à cette plateforme, j'ai trouvé le prêt parfait pour ma nouvelle maison. Je recommande vivement!"</p>
-      </div>
-
-      <div class="testimonial">
-        <img src="customer2.jpg" alt="Photo du client">
-        <p>"J'ai pu comparer différentes offres de prêt et choisir celle qui correspondait le mieux à mes besoins. Une expérience simple et efficace!"</p>
-      </div>
-    </div>
-
-    <div class="institution-section">
-      <p>Nous sommes fiers de collaborer avec les institutions financières les plus fiables et réputées du marché.</p>
-    </div>
-
-    <div class="contact-section">
-      <p>Pour toute question ou assistance, n'hésitez pas à nous contacter :</p>
-      <p>Téléphone : 123-456-7890</p>
-      <p>E-mail : info@votreplateforme.com</p>
-    </div>
-
-    <footer>
-      <ul class="footer-links">
-        <li><a href="#">Conditions d'utilisation</a></li>
-        <li><a href="#">Politique de confidentialité</a></li>
-        <li><a href="#">Mentions légales</a></li>
-      </ul>
-
-      <div class="social-icons">
-        <a href="#"><img src="facebook-icon.png" alt="Facebook"></a>
-        <a href="#"><img src="twitter-icon.png" alt="Twitter"></a>
-        <a href="#"><img src="linkedin-icon.png" alt="LinkedIn"></a>
-      </div>
-    </footer>
   </div>
 </template>
 
+
 <script>
-import HeaderComponent from '../components/HeaderComponent.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue';
 
 export default {
-  components: { HeaderComponent }
+  components: { HeaderComponent },
+  name: 'LandingPage',
 }
 </script>
 
-<style>
-/* Styles spécifiques à votre landing page */
+<style scoped>
 
-h1 {
-  text-align: center;
+.landing-page-container {
+  max-height: 100vh;
+  overflow-y: auto;
+  padding: 20px;
 }
 
-img {
+.page-title {
+  text-align: center;
+  margin-top: 20px;
+}
+
+header {
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+header h1 {
+  font-size: 64px;
+  font-weight: 800;
+}
+
+.illustration {
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+.illustration img {
   width: 100%;
-  max-width: 500px;
-  display: block;
-  margin: 0 auto;
+  max-width: 600px;
+  border-radius: 10px;
 }
 
-.info-block {
+.benefits {
   text-align: center;
-  margin-bottom: 30px;
+  margin-bottom: 40px;
+  padding: 20px;
+  background-color: rgba(45, 59, 84, 0.9);
+  border-radius: 10px;
+  margin: 20px 200px;
 }
 
-.loan-form {
-  text-align: center;
-  margin-bottom: 30px;
+.benefits h3 {
+  margin-bottom: 20px;
+  font-size: 20px;
 }
 
 .testimonials {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 30px;
+  text-align: center;
+  margin-bottom: 40px;
+  padding: 20px;
+  background-color: rgba(45, 59, 84, 0.9);
+  border-radius: 10px;
+  margin: 20px 200px;
 }
 
 .testimonial {
-  text-align: center;
-  margin: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
 }
 
 .testimonial img {
@@ -118,38 +138,73 @@ img {
   margin-bottom: 10px;
 }
 
-.institution-section {
-  text-align: center;
-  margin-bottom: 30px;
+.testimonial p {
+  margin-bottom: 10px;
+  font-style: italic;
 }
 
-.contact-section {
+.testimonial span {
+  font-weight: bold;
+}
+
+.call-to-action {
   text-align: center;
-  margin-bottom: 30px;
+  margin-bottom: 40px;
+}
+
+.cta-buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.cta-button {
+  margin: 0 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  background-color: #428bca;
+  color: #fff;
+  cursor: pointer;
+}
+
+.reassurance {
+  text-align: center;
+  margin-bottom: 40px;
+  padding: 20px;
+  background-color: rgba(45, 59, 84, 0.9);
+  border-radius: 10px;
+  margin: 20px 200px;
+}
+
+.reassurance h3 {
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+
+.partner-logos {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.partner-logos img {
+  width: 80px;
+  height: 80px;
+  margin: 0 10px;
 }
 
 footer {
   text-align: center;
-  margin-top: 50px;
+  margin-top: 40px;
+  font-size: 14px;
 }
 
-.footer-links {
-  list-style-type: none;
-  padding: 0;
-  margin-bottom: 10px;
-}
-
-.footer-links li {
-  display: inline;
-  margin-right: 10px;
-}
-
-.social-icons {
-  display: flex;
-  justify-content: center;
-}
-
-.social-icons a {
+footer a {
   margin: 0 5px;
+  text-decoration: none;
+  color: #428bca;
 }
+
 </style>
