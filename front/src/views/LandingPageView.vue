913632e2fa4d3fb8492320cba7b98c405a21382c
Romain Foucher <58913880+ReynauldTheHopeless@users.noreply.github.com>
2023-06-20 09:34:32 +0200

    Ajout de quelques images et logos, ajout du lien de redirection vers la page d'enregistrement client

--- front/src/views/LandingPageView.vue
+++ front/src/views/LandingPageView.vue
@@ -8,7 +8,7 @@
       </header>
 
       <section class="illustration">
-        <img src="XXXXXX" alt="Photo d'une famille" />
+        <img src="../assets/famille-maison.jpg" alt="Photo d'une famille" />
       </section>
 
       <section class="benefits">
@@ -22,33 +22,35 @@
       </section>
 
       <section class="testimonials">
-        <h3>Témoignages</h3>
+        <h3>Quelques témoignages de nos clients</h3>
         <div class="testimonial">
-          <img src="client1.jpg" alt="Client 1" />
           <p>"Grâce à cette plateforme, j'ai pu obtenir un prêt immobilier avec un taux avantageux et sans tracas. Je la recommande vivement !"</p>
           <span>- Jean-Michel</span>
         </div>
         <div class="testimonial">
-          <img src="client2.jpg" alt="Client 2" />
           <p>"Je suis impressionné par la facilité d'utilisation de cette plateforme. J'ai rapidement trouvé l'offre de prêt qui me convenait."</p>
           <span>- Matthieu Evuort</span>
+        </div>
+        <div class="testimonial">
+          <p>"Je suis vraiment impressionné par la facilité avec laquelle j'ai trouvé l'offre de prêt immobilier qui me convenait. C'était rapide et pratique, et cela m'a grandement simplifié la vie."</p>
+          <span>- Thomas Song</span>
         </div>
       </section>
 
       <section class="call-to-action">
         <h3>Rejoignez-nous !</h3>
         <div class="cta-buttons">
-          <button class="cta-button">Professionnels</button>
-          <button class="cta-button">Particuliers</button>
+          <button class="cta-button" @click="redirectToClientRegister">Particuliers</button>
+          <button class="cta-button" >Profesionnels</button>
         </div>
       </section>
 
       <section class="reassurance">
         <h3>Nos partenaires</h3>
         <div class="partner-logos">
-          <img src="bank1.png" alt="Logo SG" />
-          <img src="bank2.png" alt="Logo BNP Paribas" />
-          <img src="bank3.png" alt="Logo BRED" />
+          <img src="../assets/logo-societe-generale.jpg" alt="Logo SG" />
+          <img src="../assets/logo-bnp-paribas.png" alt="Logo BNP Paribas" />
+          <img src="../assets/logo-bred.png" alt="Logo BRED" />
         </div>
         <p>Plus de XXXX prêts accordés avec succès</p>
         <p>Taux de satisfaction client de XX%</p>
@@ -64,7 +66,13 @@ import HeaderComponent from '@/components/HeaderComponent.vue';
 export default {
   components: { HeaderComponent },
   name: 'LandingPage',
+    methods: {
+    redirectToClientRegister() {
+      this.$router.push({ path: '/register' });
+    }
+  }
 }
+
 </script>
 
 <style scoped>
