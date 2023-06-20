a82a020fee818736befcee19b2689e910ff9684d
PrinceFowang <PrinceFowang@users.noreply.github.com>
2023-06-20 07:34:31 +0000

    deploy: ccb03dd6ef9fedb16bcef53a41640f5755dca5cc

--- js/220.446cb045.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[220],{220:function(n,r,e){e.r(r),e.d(r,{default:function(){return o}});var t=e(3396);const u=(0,t._)("h1",null,"This is a bank home page",-1),a=[u];function s(n,r,e,u,s,c){return(0,t.wg)(),(0,t.iD)("div",null,a)}var c={},f=e(89);const i=(0,f.Z)(c,[["render",s]]);var o=i}}]);
-//# sourceMappingURL=220.446cb045.js.map
